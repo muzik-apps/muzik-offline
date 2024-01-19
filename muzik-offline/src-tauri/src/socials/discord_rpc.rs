11c19b8e91dc34db91e5c36cb7e0c1e28ff3110a
Michael <u21546551@tuks.co.za>
2024-01-19 13:53:59 +0200

    Add dotenv dependency for environment variables and refactored some discord rpc code

--- muzik-offline/src-tauri/src/socials/discord_rpc.rs
+++ muzik-offline/src-tauri/src/socials/discord_rpc.rs
@@ -1,30 +1,39 @@
+use tauri::State;
 use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
+use std::env;
+use std::sync::Mutex;
 
 pub struct DiscordRpc {
-    client_id: String,
     client: DiscordIpcClient,
     allowed_to_connect: bool,
 }
 
 impl DiscordRpc {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
-        let client_id = String::from("1197657294490583071");
+        //get client id from env variables
+        let client_id = env::var("DISCORD_CLIENT_ID").expect("DISCORD_CLIENT_ID env variable not set");
         let client: DiscordIpcClient = DiscordIpcClient::new(&client_id)?;
 
         Ok(
             Self {
-                client_id,
                 client,
                 allowed_to_connect: false,
             }
         )
     }
 
+    pub fn set_allowed_to_connect(&mut self, allowed_to_connect: bool) {
+        self.allowed_to_connect = allowed_to_connect;
+    }
+
     pub fn connect(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         if self.allowed_to_connect {
             self.client.connect()?;
+            Ok(())
         }
-        Ok(())
+        else{
+            Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "not allowed to connect to discord rpc")))
+        }
     }
 
     pub fn set_activity(&mut self, song_name: String, state: String, large_image_key: String) -> Result<(), Box<dyn std::error::Error>> {
@@ -38,21 +47,151 @@ impl DiscordRpc {
                         .large_text(&song_name)
                 );
             self.client.set_activity(activity)?;
+            Ok(())
         }
-        Ok(())
+        else{
+            Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "not allowed to connect to discord rpc")))
+        }
     }
 
     pub fn clear_activity(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         if self.allowed_to_connect {
             self.client.clear_activity()?;
+            Ok(())
         }
-        Ok(())
+        else{
+            Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "not allowed to connect to discord rpc")))
+        }
     }
 
     pub fn close(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         if self.allowed_to_connect {
             self.client.close()?;
+            Ok(())
         }
-        Ok(())
+        else{
+            Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "not allowed to connect to discord rpc")))
+        }
+    }
+}
+
+impl Drop for DiscordRpc {
+    fn drop(&mut self) {
+        //fail silently and print out failure but don't crash the application
+        //the OS will handle memory cleanup since the application is returning the resources it had allocated anyways
+        if self.allowed_to_connect {
+            match self.client.close(){
+                Ok(_) => {},
+                Err(e) => {
+                    println!("error while closing discord rpc: {}", e);
+                }
+            }
+        }
+    }
+}
+
+#[tauri::command]//this will run when the user sets discord rpc to enabled
+pub fn allow_connection_and_connect_to_discord_rpc(discord_rpc: State<Mutex<DiscordRpc>>) -> Result<String, String> {
+    match discord_rpc.lock(){
+        Ok(mut discord_rpc) => {
+            discord_rpc.set_allowed_to_connect(true);
+            match discord_rpc.connect(){
+                Ok(_) => {
+                    Ok(String::from("connected to discord rpc"))
+                },
+                Err(e) => {
+                    Err(format!("error while connecting to discord rpc: {}", e))
+                }
+            }
+        },
+        Err(e) => {
+            Err(format!("error while locking discord rpc mutex: {}", e))
+        }
+    }
+}
+
+#[tauri::command]//this will only ever run once when the application starts
+pub fn attempt_to_connect_if_possible(discord_rpc: State<Mutex<DiscordRpc>>) -> Result<String, String> {
+    match discord_rpc.lock(){
+        Ok(mut discord_rpc) => {
+            match discord_rpc.connect(){
+                Ok(_) => {
+                    Ok(String::from("connected to discord rpc"))
+                },
+                Err(e) => {
+                    Err(format!("error while connecting to discord rpc: {}", e))
+                }
+            }
+        },
+        Err(e) => {
+            Err(format!("error while locking discord rpc mutex: {}", e))
+        }
+    }
+}
+
+#[tauri::command]//this will run when the user sets discord rpc to disabled
+pub fn disallow_connection_and_close_discord_rpc(discord_rpc: State<Mutex<DiscordRpc>>) -> Result<String, String> {
+    match discord_rpc.lock(){
+        Ok(mut discord_rpc) => {
+            match discord_rpc.close(){
+                Ok(_) => {
+                    discord_rpc.set_allowed_to_connect(false);
+                    Ok(String::from("closed discord rpc"))
+                },
+                Err(e) => {
+                    Err(format!("error while closing discord rpc: {}", e))
+                }
+            }
+        },
+        Err(e) => {
+            Err(format!("error while locking discord rpc mutex: {}", e))
+        }
+    }
+}
+
+#[tauri::command]//this will run when the user changes the song they are listening to
+pub fn set_discord_rpc_activity(discord_rpc: State<Mutex<DiscordRpc>>, song_name: String, state: String, large_image_key: String) -> Result<String, String> {
+    match discord_rpc.lock(){
+        Ok(mut discord_rpc) => {
+            match discord_rpc.clear_activity(){
+                Ok(_) => {
+                    
+                },
+                Err(e) => {
+                    return Err(format!("error while clearing discord rpc activity: {}", e));
+                }
+            }
+
+            match discord_rpc.set_activity(song_name, state, large_image_key){
+                Ok(_) => {
+                    Ok(String::from("set discord rpc activity"))
+                },
+                Err(e) => {
+                    Err(format!("error while setting discord rpc activity: {}", e))
+                }
+            }
+        },
+        Err(e) => {
+            Err(format!("error while locking discord rpc mutex: {}", e))
+        }
+    }
+}
+
+#[tauri::command]//this will run just before the user changes the song they are listening to or when they stop listening to music
+pub fn clear_discord_rpc_activity(discord_rpc: State<Mutex<DiscordRpc>>) -> Result<String, String> {
+    match discord_rpc.lock(){
+        Ok(mut discord_rpc) => {
+            match discord_rpc.clear_activity(){
+                Ok(_) => {
+                    Ok(String::from("cleared discord rpc activity"))
+                },
+                Err(e) => {
+                    Err(format!("error while clearing discord rpc activity: {}", e))
+                }
+            }
+        },
+        Err(e) => {
+            Err(format!("error while locking discord rpc mutex: {}", e))
+        }
     }
 }
